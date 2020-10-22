597497759039464d65028550f84480c829d5d966
Nicholas Gibson <nckgibson4@gmail.com>
2020-10-22 12:14:31 -0700

    Initial commit

---
+++ BlazorGames/Models/ConnectFour/GamePiece.cs
@@ -0,0 +1,16 @@
+using BlazorGames.Models.ConnectFour.Enums;
+namespace BlazorGames.Models.ConnectFour
+{
+    public class GamePiece
+    {
+        public PieceColor Color;
+        public GamePiece()
+        {
+            Color = PieceColor.Blank;
+        }
+        public GamePiece(PieceColor color)
+        {
+            Color = color;
+        }
+    }
+}
